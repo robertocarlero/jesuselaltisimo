12ab4e5cacff205dd29ac3d2f70841a50ae4e016
mariana <mariana@painfreedentalmarketing.com>
2025-05-22 10:12:39 -0400

    build: :building_construction: Prepare enviromento for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
+
+import playformCompress from "@playform/compress";
+
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://jesuselaltisimo.org",
+
   vite: {
     plugins: [tailwindcss()],
   },
@@ -16,5 +25,14 @@ export default defineConfig({
     icon({
       iconDir: "src/assets/icons",
     }),
+    sitemap(),
+    robotsTxt(),
+    playformCompress(),
   ],
+
+  adapter: netlify({
+    edgeMiddleware: true,
+    cacheOnDemandPages: true,
+    imageCDN: true,
+  }),
 });
